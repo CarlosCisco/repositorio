0bc7f2a738fd59d46fb41b955fdca8d80310a7db
carlos <cgermanrojas@gmail.com>
2021-10-01 08:52:03 -0500

    version 2 1 de octubre

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,5 @@
 print("Hola mundo")
 print("Hola mundo2")
 print("Hola mundo3")
+print("Hola mundo4")
+print("Hola mundo5")
--- ejemplo_dos.py
+++ ejemplo_dos.py
@@ -1,3 +1,4 @@
 print(4+6)
 print("Hola")
 print("hola3")
+print("Hola mundo4")
